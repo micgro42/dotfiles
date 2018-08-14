6a11d067cbcbb01ad83f472d327cdff525803e3c
Michael Große <mic.grosse@googlemail.com>
2018-08-14 21:07:34 +0200

    💡 Add more documentation to the install script

--- install.sh
+++ install.sh
@@ -6,24 +6,29 @@ HERE=`pwd`
 
 function installDotfile {
 
+        # If no filename is provided to the function report an error and exit
         if [[ -z ${1+x} ]]; then
                 echo "no filename given!"
                 return
         fi
 
+        # if the filename doesn't exist report an error and exit
         if [[ ! -e $1 ]]; then
                 echo "file $1 does not exist locally!"
                 return
         fi
 
+        # if the destination exists and is a symlink, then remove it
         if [[ -h "$HOME/.$1" ]]; then
             rm -f "$HOME/.$1"
         fi
 
+        # if the destination exists then move it to a backup
         if [[ -e "$HOME/.$1" ]]; then
             mv "$HOME/.$1" "$HOME/.${1}_bak"
         fi
 
+        # link the filename to the home directory
         ln -s "$HERE/$1" "$HOME/.$1"
 
 }
